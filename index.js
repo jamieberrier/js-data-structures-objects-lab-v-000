9ec1f5fb36049fe0ab8431d2fb7659f9394c6efe
Jamie Berrier <berrier.jamie@gmail.com>
2020-06-08 15:39:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ This should all not actually mutate the driver passed in. */
 function deleteFromDriverByKey(driver, key) {
   const newDriver = Object.assign({}, driver);
   console.log(newDriver['key']);
-  delete newDriver[key];
+  delete newDriver['key'];
 }
 
 /* this function should work the same as deleteFromDriverByKey() but it should mutate the driver passed in.
