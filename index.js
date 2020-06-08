9ac47d263b634ef308c12c7d84d2617033d2ed77
Jamie Berrier <berrier.jamie@gmail.com>
2020-06-08 15:50:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,6 @@ function deleteFromDriverByKey(driver, key) {
 /* this function should work the same as deleteFromDriverByKey() but it should mutate the driver passed in.
 Be sure and consider whether dot-notation or bracket-notation might affect your solution. */
 function destructivelyDeleteFromDriverByKey(driver, key) {
-
+  delete driver[key];
+  return driver;
 }
